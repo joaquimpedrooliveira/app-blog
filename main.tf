600b2acbd93b1e062f4d7eff4bcdada5943f4533
Joaquim Oliveira <joaquim-pedro.oliveira@serpro.gov.br>
2020-06-14 17:00:21 -0300

    Versão inicial do projeto.

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+module "servico_wordpress" {
+  source                = "git::https://github.com/joaquimpedrooliveira/infra-modulo-app-php.git"
+
+  project_name          = "blog"
+
+  db_name               = "wordpress_db"
+  db_user               = "wordpress_user"
+  db_password           = "12345"  # No mundo real, deve estar externalizado
+  db_instance_type      = "t2.micro"
+
+  app_instance_type     = "t2.micro"
+  app_playbook_filename = "provision_wordpress.yml"
+  app_config_dir        = "ansible"
+
+  ssh_key_name          = "aula-terraform"
+  allowed_ssh_ips       = ["192.168.1.1/32"]
+}
+
+output "endereco_wordpress" {
+  value = module.servico_wordpress.app_public_dns
+}
